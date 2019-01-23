20575d4ed550c71f8854677dceda9c9f9a586427
arismoratalla <arismoratalla@gmail.com>
2019-01-23 10:28:03 +0800

    Update main.php

--- common/config/main.php
+++ common/config/main.php
@@ -14,7 +14,8 @@ return [
         'gridview' => ['class' => 'kartik\grid\Module'],
         'gii' => [
             'class' => 'yii\gii\Module',
-            'allowedIPs' => ['127.0.0.1', '::1', '192.168.1.*'] // adjust this to your needs
+            //'allowedIPs' => ['127.0.0.1', '::1', '192.168.1.*'] // adjust this to your needs
+            'allowedIPs' => ['*'] // adjust this to your needs
         ],
     ],
     
