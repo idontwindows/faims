ad10d10da04c4d1e8277fd1b8dd90c90cf320c55
markherras2003 <larrymark2003@gmail.com>
2019-08-28 11:55:38 +0800

    Update db.php

--- common/config/db.php
+++ common/config/db.php
@@ -15,7 +15,7 @@ return [
 
     'procurementdb'=>[
         'class' => 'yii\db\Connection',  
-        //'dsn' => 'mysql:host=localhost;dbname=fais-procurement',
+        //'dsn' => 'mysql:host=localhost;dbname=fais-procurement',cc
         'dsn' => 'mysql:host=192.168.1.96;dbname=fais-procurement',
         'username' => 'fais',
         'password' => 'D057R3g10n9!@#$%',
