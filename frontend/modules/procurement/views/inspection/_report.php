e07ad4434c05891c000f61d4058175ded2dbfbd0
markherras2003 <larrymark2003@gmail.com>
2019-09-24 16:47:54 +0800

    Update _report.php

--- frontend/modules/procurement/views/inspection/_report.php
+++ frontend/modules/procurement/views/inspection/_report.php
@@ -12,7 +12,7 @@
         $append = "<tr class=\"nospace-border\" style='height: 200px;'>";
         $append = $append . "<td width='12%' style='padding-left: 25px;vertical-align: top;'>" . $quantity . "</td>";
         $append = $append . "<td width='10%' style='padding-left: 5px;vertical-align: top;'>units</td>";
-        $append = $append . "<td width='48%' style='text-align: justify;vertical-align: top'>" . $itemdescription . "</td>";
+        $append = $append . "<td width='48%' style='text-align: justify;vertical-align: top' autosize='0'>" . $itemdescription . "</td>";
         $append = $append . "<td width='18%' style='padding-left: 50px;text-align: right;'>" . number_format($totalcost,2) . "</td>";
         $append = $append . "</tr>";
         $fin = $fin . $append;
@@ -25,8 +25,6 @@
             echo $fin;
         ?>
     </table>
-
-
     </div>
 
 </div>
