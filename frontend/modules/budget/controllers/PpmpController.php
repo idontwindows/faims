740fc9a2966858cd1e2c3d3133ed868a18f773d7
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2019-10-14 16:01:32 +0800

    Adding Budget Module
    Initial Code for budget module

---
+++ frontend/modules/budget/controllers/PpmpController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace frontend\modules\budget\controllers;
+
+use Yii;
+use common\models\procurement\Division;
+use common\models\procurement\Section;
+use common\models\procurement\Unit;
+
+use common\models\procurementplan\Item;
+use common\models\procurementplan\Ppmp;
+use common\models\procurementplan\Ppmpitem;
+use common\models\procurementplan\PpmpSearch;
+
+use yii\data\ActiveDataProvider;
+use yii\data\ArrayDataProvider;
+use yii\db\Query;
+
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
+
+class PpmpController extends Controller
+{
+//    public function actionIndex()
+//    {
+//        return $this->render('index');
+//    }
+
+    public function actionIndex()
+    {
+        if(isset($_GET['year']))
+            $selected_year = $_GET['year'];
+        else 
+            $selected_year = '2019';
+        //$selected_year = isset($_POST['year']) ? $_POST['year'] : '';
+        
+        $searchModel = new PpmpSearch();
+        //$dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        
+        $divisions = Division::find()->orderBy('name')->asArray()->all();
+        $listDivisions = ArrayHelper::map($divisions, 'division_id', 'name');
+        
+        $sections = Section::find()->orderBy('division_id, section_id');
+        $dataProvider = Ppmp::find();
+        
+        $dataProvider = new ActiveDataProvider([
+            'query' => $dataProvider,
+            'pagination' => [
+                'pageSize' => 20,
+            ],
+        ]);
+    
+        
+        $ppmpDataProvider = new ActiveDataProvider([
+            'query' => $sections,
+            'pagination' => [
+                'pageSize' => 20,
+            ],
+        ]);
+
+        $queryPpmpItems = Ppmpitem::find();
+        $ppmpItemsDataProvider = new ActiveDataProvider([
+            'query' => $queryPpmpItems,
+            'pagination' => [
+                'pageSize' => 10,
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    //'created_at' => SORT_DESC,
+                    //'title' => SORT_ASC, 
+                ]
+            ],
+        ]);
+        
+        $units = Unit::find()->orderBy('name')->asArray()->all();
+        $listUnits = ArrayHelper::map($units, 'unit_id', 'name');
+        
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+            'ppmpDataProvider' => $ppmpDataProvider,
+            'ppmpItemsDataProvider' => $ppmpItemsDataProvider,
+            'listDivisions' => $listDivisions,
+            
+            'listUnits' => $listUnits,
+            'selected_year' => $selected_year,
+        ]);
+    }
+
+}
---
+++ frontend/modules/budget/views/ppmp/index.php
@@ -0,0 +1,120 @@
+<?php
+use kartik\helpers\Html;
+use yii\helpers\Url;
+use yii\helpers\ArrayHelper;
+
+use kartik\datecontrol\DateControl;
+use kartik\detail\DetailView;
+use kartik\editable\Editable; 
+use kartik\grid\GridView;
+
+use yii\bootstrap\Modal;
+
+use common\models\procurementplan\Section;
+use common\models\procurementplan\Ppmp;
+use common\models\procurementplan\PpmpSearch;
+
+//use common\models\procurement\Division;
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\procurementplan\PpmpSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Ppmps';
+$this->params['breadcrumbs'][] = $this->title;
+
+// Modal PPMP
+Modal::begin([
+    'header' => '<h4 id="modalHeader" style="color: #ffffff"></h4>',
+    'id' => 'modalPpmp',
+    'size' => 'modal-md',
+    'options'=> [
+             'tabindex'=>false,
+        ],
+]);
+
+echo "<div id='modalContent'><div style='text-align:center'><img src='/images/loading.gif'></div></div>";
+Modal::end();
+
+Modal::begin([
+    'header' => '<h4 id="modalHeader" style="color: #ffffff"></h4>',
+    'id' => 'modalPpmpItem',
+    'size' => 'modal-lg',
+    'options'=> [
+             'tabindex'=>false,
+        ],
+]);
+
+echo "<div id='modalContent'><div style='text-align:center'><img src='/images/loading.gif'></div></div>";
+Modal::end();
+?>    
+    <div class="ppmp-index">
+    <?php //echo $selected_year; ?>
+    <h3 style="text-align: center"><?= Html::encode('PROJECT PROCUREMENT MANAGEMENT PLAN (PPMP)') ?></h3>
+
+   
+   
+        <?php
+        echo GridView::widget([
+            'id' => 'ppmp3',
+            'dataProvider' => $ppmpDataProvider,
+            'columns' => [
+                            [
+                                'attribute'=>'division', 
+                                'width'=>'250px',
+                                'contentOptions' => ['style' => 'max-width:25px;white-space:normal;word-wrap:break-word;font-weight:bold'],
+                                'value'=>function ($model, $key, $index, $widget) use ($selected_year) { 
+                                    return $model->division->name;
+                                },
+                                'group'=>true,  // enable grouping,
+                                'groupedRow'=>true,                    // move grouped column to a single grouped row
+                                'groupOddCssClass'=>'kv-grouped-row',  // configure odd group cell css class
+                                'groupEvenCssClass'=>'kv-grouped-row', // configure even group cell css class
+                            ],
+                            [
+                                'attribute'=>'name',
+                                'contentOptions' => ['style' => 'padding-left: 25px'],
+                                'width'=>'250px',
+                                'value'=>function ($model, $key, $index, $widget) { 
+                                    return $model->name;
+                                },
+                            ],
+                            [
+                                'attribute'=>'status',
+                                'header'=>'PPMPs',
+                                'width'=>'250px',
+                                'headerOptions' => ['style' => 'text-align: center'],
+                                'contentOptions' => ['style' => 'text-align: center'],
+                                'format' => 'raw',
+                                'value'=>function ($model) { 
+                                    return $model->getPpmps();
+                                },
+                            ],
+                    ],
+            'containerOptions' => ['style' => 'overflow: auto'], // only set when $responsive = false
+            'headerRowOptions' => ['class' => 'kartik-sheet-style'],
+            'filterRowOptions' => ['class' => 'kartik-sheet-style'],
+            'pjax' => true, // pjax is set to always true for this demo
+            'panel' => [
+                'type' => GridView::TYPE_PRIMARY,
+                //'heading' => $heading,
+            ],
+            // set your toolbar
+            'toolbar' => 
+                        [
+                            [
+                                'content'=>
+                                    Html::button('PENDING', ['title' => 'Approved', 'class' => 'btn btn-warning', 'style'=>'width: 90px; margin-right: 6px;']) .    
+                                    Html::button('SUBMITTED', ['title' => 'Approved', 'class' => 'btn btn-primary', 'style'=>'width: 90px; margin-right: 6px;']) .
+                                    Html::button('APPROVED', ['title' => 'Approved', 'class' => 'btn btn-success', 'style'=>'width: 90px; margin-right: 6px;'])
+                            ],
+                            //'{export}',
+                            //'{toggleData}'
+                        ],
+            
+            'toggleDataOptions' => ['minCount' => 10],
+            //'exportConfig' => $exportConfig,
+            'itemLabelSingle' => 'item',
+            'itemLabelPlural' => 'items'
+        ]);
+        ?>
+</div>
