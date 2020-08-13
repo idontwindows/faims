4a49c879d39407c9f351691037b8599293e0899a
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2020-08-13 22:53:43 +0800

    minor fix

--- frontend/web/js/finance/ajax-modal-popup.js
+++ frontend/web/js/finance/ajax-modal-popup.js
@@ -43,6 +43,59 @@ $("body").on("click","#buttonUploadAttachments",function () {
     loadModal($(this).attr('value'));
 });
 
-$("body").on("click","#buttonSubmit",function () {
+$("body").on("click","#buttonSubmitForVerification",function () {
     loadModal($(this).attr('value'));
+});
+
+$("body").on("click","#buttonSubmitForValidation",function () {
+    loadModal($(this).attr('value'));
+});
+
+$("body").on("click","#buttonValidateRequest",function () {
+    loadModal($(this).attr('value'));
+});
+
+$("body").on("click","#buttonAddAllotment",function () {
+    loadModal($(this).attr('value'));
+});
+
+$("body").on("click","#buttonObligate",function () {
+    loadModal($(this).attr('value'));
+});
+
+$("body").on("click","#buttonCertifyfundsavailable",function () {
+    loadModal($(this).attr('value'));
+});
+
+$("body").on("click","#buttonApprove",function () {
+    loadModal($(this).attr('value'));
+});
+
+$("body").on("click","#buttonGenerateOSNumber",function () {
+    loadModal($(this).attr('value'));
+});
+
+$("body").on("click","#buttonGenerateDVNumber",function () {
+    loadModal($(this).attr('value'));
+});
+
+
+$("body").on("click","#buttonAddAccounttransaction",function () {
+    loadModal($(this).attr('value'));
+});
+
+//$("body").on("click","#buttonComment",function () {
+    //loadModal();
+    //alert($(this).attr('title'));
+    //return false;
+//});
+
+$("body").on("click","#buttonCreateOsdv",function () {
+    $('#modalRequest').modal('show')
+        .find('#modalContent')
+        .load($(this).attr('value'));
+    $('#modalHeader').html($(this).attr('title'));
+    setTimeout(function () {
+        $("#btnrefresh").click();
+    },1500);
 });
