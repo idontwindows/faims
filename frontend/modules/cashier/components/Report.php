eb3a05011d4e36fbe610515aead7154a52de1dec
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2020-09-24 14:53:11 +0800

    Update Report.php

--- frontend/modules/cashier/components/Report.php
+++ frontend/modules/cashier/components/Report.php
@@ -128,7 +128,7 @@ class Report {
             $template .= "<tr>";
             $template .= "<td style='border-bottom: 1px solid #000; border-right: 1px solid #000;'>".$item->name."</td>";
             $template .= "<td style='text-align: center; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".$item->creditor->account_number."</td>";
-            $template .= "<td style='text-align: center; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".$item->osdv->os->os_number."</td>";
+            $template .= "<td style='text-align: center; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".$item->osdv->os ? $item->osdv->os->os_number : '-'."</td>";
             $template .= "<td style='text-align: center; border-bottom: 1px solid #000; border-right: 1px solid #000;'>000</td>";
             $template .= "<td style='text-align: right; padding-right: 10px; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".$fmt->asDecimal($item->gross_amount)."</td>";
             $template .= "<td style='text-align: right; padding-right: 10px; border-bottom: 1px solid #000; border-right: 1px solid #000;'>".$item->gross_amount."</td>";
