ac5dd7c9313725235a05882695ea47146e261752
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2020-10-13 03:30:31 +0800

    Update Notification.php

--- common/components/Notification.php
+++ common/components/Notification.php
@@ -36,7 +36,7 @@ class Notification extends Component {
         {
             Yii::$app->mailer->compose()
             //Yii::$app->mailer->compose(['html' => 'html', 'text' => 'passwordResetToken-text'], ['user' => 'aris'])
-            ->setFrom([Yii::$app->params['supportEmail'] => Yii::$app->name . ' Mailer'])
+            ->setFrom([Yii::$app->params['supportEmail'] => 'FAIMS Mailer'])
             ->setTo($recipients[$i])
             ->setSubject($title)
             ->setHtmlBody($message)
