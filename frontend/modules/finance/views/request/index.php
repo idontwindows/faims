188f9dca96d5dfdd0aa9de8a7ca32f2430fa3f43
arismoratalla <35106073+arismoratalla@users.noreply.github.com>
2020-10-16 12:06:27 +0800

    Update index.php

--- frontend/modules/finance/views/request/index.php
+++ frontend/modules/finance/views/request/index.php
@@ -132,7 +132,7 @@ Modal::end();
                                 'attribute'=>'created_by',
                                 'headerOptions' => ['style' => 'text-align: center;'],
                                 'contentOptions' => ['style' => 'text-align: center; vertical-align:middle; '],
-                                'width'=>'250px',
+                                'width'=>'150px',
                                 'value'=>function ($model, $key, $index, $widget) { 
                                     //return Profile::find($model->created_by)->one()->fullname;
                                     return $model->profile->fullname;
